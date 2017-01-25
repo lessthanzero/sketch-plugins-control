a1241bede822b97b4bcb0962e1cd079c7a04d553
sashakatin <aleksandr.katin@gmail.com>
2017-01-26 01:43:19 +0300

    start repo

---
+++ sketch-plugins-control.sh
@@ -0,0 +1,6 @@
+cd ~/Library/Application Support/com.bohemiancoding.sketch3/Plugins;
+git clone https://github.com/romashamin/compo-sketch.git;
+git clone https://github.com/utom/sketch-measure.git;
+git clone https://github.com/zeplin/zeplin-sketch-plugin.git;
+git clone https://github.com/bouchenoiremarc/Sketch-Constraints.git;
+gitup --add Formr; gitup --add Sketch-Constraints; gitup --add compo-sketch; gitup --add sketch-measure; gitup --add zeplin-sketch-plugin;
