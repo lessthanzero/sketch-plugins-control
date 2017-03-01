d735b4ce2ab3e843717a032c498cbc28c03b912c
Sasha Katin <aleksandr.katin@gmail.com>
2017-03-01 15:41:00 +0300

    update list of plugins

--- sketch-plugins-control.sh
+++ sketch-plugins-control.sh
@@ -3,4 +3,7 @@ git clone https://github.com/romashamin/compo-sketch.git;
 git clone https://github.com/utom/sketch-measure.git;
 git clone https://github.com/zeplin/zeplin-sketch-plugin.git;
 git clone https://github.com/bouchenoiremarc/Sketch-Constraints.git;
-gitup --add Formr; gitup --add Sketch-Constraints; gitup --add compo-sketch; gitup --add sketch-measure; gitup --add zeplin-sketch-plugin;
+git clone https://github.com/AnimaApp/Auto-Layout.git;
+git clone https://github.com/Volorf/Sparkliner.git;
+gitup --add Formr; gitup --add Sketch-Constraints; gitup --add compo-sketch; gitup --add sketch-measure; gitup --add zeplin-sketch-plugin; gitup --add Auto-Layout; gitup --add Sparkliner;
+
